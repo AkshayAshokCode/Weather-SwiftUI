95bbd892724d8b583d5869c07fc925defb3c2b45
akshay ashok <akshayashokan1054@gmail.com>
2024-01-18 06:17:17 +0530

    add atributed string

--- Weather-SwiftUI/Views/Main/HomeView.swift
+++ Weather-SwiftUI/Views/Main/HomeView.swift
@@ -20,12 +20,13 @@ struct HomeView: View {
             Image("House").frame(maxWidth: .infinity, alignment: .top)
                 .padding(.top, 257)
             
-            VStack{
+            VStack(spacing: -10){
                 Text("Montreal")
                     .font(.largeTitle)
                 
                 VStack{
-                    Text("19°" + "\n" + "Mostly Clear")
+//
+                    Text(attributedString)
                     
                     Text("H:24°    L:18°")
                         .font(.title3.weight(.semibold))
@@ -33,6 +34,26 @@ struct HomeView: View {
                 Spacer()
             }.padding(.top, 51)
         }
+    }
+    
+    private var attributedString: AttributedString{
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        if let temp = string.range(of: "19°"){
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | "){
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+    
+        
+        if let weather = string.range(of: "Mostly Clear"){
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        return string
     }
 }
 
